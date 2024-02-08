7b05bfd24dadba928498d6286165330223a06cf7
JunheumNa <155754531+JunheumNa@users.noreply.github.com>
2024-02-08 22:58:07 +0900

    Add files via upload

--- js/skeleton.js
+++ js/skeleton.js
@@ -1,9 +1,12 @@
-// Function to calculate loading time and show/hide skeleton screen
+// Show the skeleton screen
+document.getElementById('skeleton-screen').style.display = 'flex';
+
+// Function to calculate loading time and hide skeleton screen
 window.addEventListener('load', function() {
   var startTime = performance.now(); // Get start time when the page starts loading
   
-  // Function to calculate and display loading time
-  function calculateAndDisplayLoadingTime() {
+  // Function to hide skeleton screen and display loading time
+  function hideSkeletonScreen() {
     var endTime = performance.now(); // Get end time when the page finishes loading
     var loadingTime = endTime - startTime; // Calculate loading time in milliseconds
     console.log('Page loaded in ' + loadingTime + ' milliseconds');
@@ -14,11 +17,6 @@ window.addEventListener('load', function() {
       document.getElementById('skeleton-screen').classList.add('fade-out');
     }, skeletonScreenTimeout);
   }
-  
-  // Check if all content (including images, scripts, etc.) has loaded
-  if (document.readyState === 'complete') {
-    calculateAndDisplayLoadingTime();
-  } else {
-    window.addEventListener('load', calculateAndDisplayLoadingTime);
-  }
+
+  hideSkeletonScreen();
 });
