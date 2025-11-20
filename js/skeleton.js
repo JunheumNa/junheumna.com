f0c5d8942179a3553ed9eee120f54502f063b535
JunheumNa <155754531+JunheumNa@users.noreply.github.com>
2025-11-20 09:23:55 +0900

    Delete js directory

--- js/skeleton.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('load', function() {
-  // Hide the skeleton screen after the page has finished loading
-  var skeletonScreen = document.getElementById('skeleton-screen');
-  skeletonScreen.classList.add('fade-out');
-});
